2495b0aae9051d6bd4d5dbf6cb95ed757e4125a9
Seint Myat Noe <seintnoe@gmail.com>
2025-02-25 23:02:15 +0630

    Side bar and design styles changes and allocation tables

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,4 +11,16 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+    public function allocation()
+    {
+        return view('admin.allocation');
+    }
+    public function assignedlists()
+    {
+        return view('admin.assignedlists');
+    }
+    public function reallocation()
+    {
+        return view('admin.reallocation');
+    }
 }
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,9 +3,12 @@ $user = auth()->user();
 @endphp
 
 @auth
+
 <div class="header">
     <nav class="navbar navbar-light bg-light justify-content-between px-4">
-        <a class="navbar-brand">Navbar</a>
+    <h1 class="fs-4 ms-2">
+                <span class="text-dark fw-bold">TripleEDU</span>
+            </h1>
         <form action="{{ route('logout') }}" method="POST">
             @csrf
             <button class="btn btn-danger">Logout</button>
