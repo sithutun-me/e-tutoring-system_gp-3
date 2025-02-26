1e645ba82c89ff8d9c16cfa1c9a7f60d13740d7e
Nandar Kyaw Win <nandarkyawwin1999@gmail.com>
2025-02-26 20:33:10 +0630

    dashboardnamechange

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Http\Controllers\AdminController;
-use Illuminate\Support\Facades\Route;
-
-
-// API Route
-Route::get('/student-inactivity', [AdminController::class, 'getInactiveStudentsData']);
