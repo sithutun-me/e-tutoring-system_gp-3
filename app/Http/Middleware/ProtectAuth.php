02294d9a610ae9077a6008aa76188652e6bc81ab
sithutun-me <zickkotun@gmail.com>
2025-03-19 00:10:04 +0630

    protect browser back button

--- app/Http/Middleware/ProtectAuth.php
+++ app/Http/Middleware/ProtectAuth.php
@@ -15,11 +15,15 @@ class ProtectAuth
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $response = $next($request);
-        $response->headers->set('Cache-control','no-store, no-cache, must-revalidate, max-age=0');
-        $response->headers->set('Pragma','no-cache');
-        $response->headers->set('Expires','Sat, 01 Jan 2000 00:00:00 GMT');
+        // $response = $next($request);
+        // $response->headers->set('Cache-control','no-store, no-cache, must-revalidate, max-age=0');
+        // $response->headers->set('Pragma','no-cache');
+        // $response->headers->set('Expires','Sat, 01 Jan 2000 00:00:00 GMT');
 
-        return $response;
+        // return $response;
+        $response = $next($request);
+        return $response->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
     }
 }
