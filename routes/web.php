9e1ccc67a57c0939a0300601263b4e3d06833780
sithutun-me <zickkotun@gmail.com>
2025-03-19 00:01:34 +0630

    Prevent Back Button

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\TutorController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['guest'])->group(function(){
+Route::middleware(['guest','protect_auth'])->group(function(){
 
     Route::get('/', function () {
         return view('login');
@@ -19,7 +19,7 @@ Route::middleware(['guest'])->group(function(){
 
 });
 
-Route::middleware('auth')->group(function(){
+Route::middleware(['auth','protect_auth'])->group(function(){
 
     // Admin Dashboard Route
     Route::middleware(['role:3'])->group(function () {
@@ -60,10 +60,10 @@ Route::middleware('auth')->group(function(){
         Route::get('/tutor/blogging', [TutorController::class, 'blogging'])->name('tutor.blogging');
         Route::get('/tutor/createposts', [TutorController::class, 'createposts'])->name('tutor.createposts');
         Route::get('/tutor/updateposts', [TutorController::class, 'updateposts'])->name('tutor.updateposts');
-        
+
         //create meeting
         Route::post('/tutor/meetingdetail', [TutorController::class, 'save'])->name('save');
-        
+
         //update meeting
         Route::put('/tutor/meetingdetail/{id}', [TutorController::class, 'save'])->name('update');
 
@@ -78,7 +78,7 @@ Route::middleware('auth')->group(function(){
     // Student Dashboard Route
     Route::middleware(['role:1'])->group(function () {
         Route::get('/student/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
-        
+
         Route::get('/student/meetinglists', [StudentController::class, 'meetinglists'])->name('student.meetinglists');
         //show create form
         Route::get('/student/meetingdetail', [StudentController::class, 'meetingdetail'])->name('student.meetingdetail.create');
